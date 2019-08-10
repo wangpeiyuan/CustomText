26bfebba4d81eed4b3cb8ef500a71f3452a8f067
wangpeiyuan <wpy0830@gmail.com>
2019-08-10 14:12:46 +0800

    feat: init

---
+++ app/src/androidTest/java/com/wangpeiyuan/customtext/customtext/demo/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.wangpeiyuan.customtext.customtext.demo
+
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.wangpeiyuan.customtext.customtext.demo", appContext.packageName)
+    }
+}
